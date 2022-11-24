2d29841ba43198d2b0a1b5696dffa5c4b8a89c98
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-11-24 09:49:47 -0300

    resolve: delete route with pass and bd connect

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -5,5 +5,5 @@ import {connect} from 'mongoose';
 
 
 export const connectToMongoDb = async () => {
-  await connect(`mongodb+srv://leonardo:kiUs2E0eW8DEUUGo@cluster0.puymz.mongodb.net/?retryWrites=true&w=majority`).catch(err => console.log(err, "Mongodb error connection"))
+  await connect(``).catch(err => console.log(err, "Mongodb error connection"))
 }
